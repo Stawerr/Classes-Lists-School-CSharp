ee71f3a5bbf6ae1fc26f635669ad810aa0b88196
Stawerr <renato-silva_16@hotmail.com>
2022-03-04 18:52:44 +0000

    pessoa

---
+++ Ficha4/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficha4
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Altura { get; set; }
+
+        public Pessoa()
+        {
+        }
+        public Pessoa(string nome, DateTime dataNascimento, int altura)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Altura = altura;
+        }
+        public Pessoa(Pessoa p)
+        {
+            Nome = p.Nome;
+            DataNascimento = p.DataNascimento;
+            Altura = p.Altura;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
